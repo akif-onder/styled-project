93e8bc6151b0c477ae7c963577cd51b59bb3725a
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-05 22:54:25 +0300

    themes continued

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,31 @@
 import Button from "./styles/Button.styled";
-import StyledHeader from "./styles/Header.styled";
+import Container from "./styles/Container.styled";
+import Flex from "./styles/Flex.styled";
+import StyledHeader, { Logo, Nav, Image } from "./styles/Header.styled";
 const Header = () => {
   return (
     <StyledHeader>
-      <div>
-        <Button color="#a62440">Apply Courses</Button>
-        <Button bg="#a62440">Talk To Advisers</Button>
-      </div>
+      <Container>
+        <Nav>
+          <Logo src="./images/logo.png"></Logo>
+          <div>
+            <Button color="#a62440">Apply Courses</Button>
+            <Button bg="#a62440">Talk To Advisers</Button>
+          </div>
+        </Nav>
+        <Flex>
+          <div>
+            <h1>The IT Career of Your Dreams Starts Here!</h1>
+            <p>
+              Clarusway is a leading international software Bootcamp. Join a
+              micro class online with other trainees and learn coding skills
+              with a highly-skilled instructor.
+            </p>
+            <Button bg="#a62440">Start Your New Career</Button>
+          </div>
+          <Image src="./images/hero.jpg"></Image>
+        </Flex>
+      </Container>
     </StyledHeader>
   );
 };
--- src/components/styles/Header.styled.jsx
+++ src/components/styles/Header.styled.jsx
@@ -1,9 +1,35 @@
 import styled from "styled-components";
 
-
 const StyledHeader = styled.header`
+  background-color: ${({ theme }) => theme.colors.header};
   padding: 4rem 0;
-  background-color: #fff;
 `;
+
+export const Logo = styled.img`
+  width: 300px;
+  padding: 4rem 0;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media (max-width: ${({theme})=> theme.responsive}){
+    flex-direction: column;
+  }
+`;
+
+
+
+export const Image = styled.img`
+  width: 350px;
+  margin: 0 2rem;
+  @media (max-width: ${({theme})=> theme.responsive}){
+    margin: 3rem 2rem;
+  }
+  
+`;
+
+
 
 export default StyledHeader;
