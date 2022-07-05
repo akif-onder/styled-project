8c22e25540801988b961759fc6466f85dffe7332
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-05 23:04:12 +0300

    GlobalStyles added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import { ThemeProvider } from "styled-components";
+import { GlobalStlyes } from "./components/styles/Global";
 
 const theme = {
   colors:{
@@ -13,6 +14,7 @@ const theme = {
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStlyes/>
       <Header />
     </ThemeProvider>
   );
